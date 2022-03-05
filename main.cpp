7ea8488b3650681a104dae6aa3041a58386319e6
joegasewicz <joegasewicz@gmail.com>
2022-03-05 21:23:19 +0000

    png

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::Window window;
-    window.create(sf::VideoMode(800, 600), "Green Dragon");
+    sf::RenderWindow window;
+    sf::Texture wizard_texture;
+    sf::Sprite wizard_sprite;
+
+    if (!wizard_texture.loadFromFile("assets/wizard-32px.png"))
+    {
+        std::cout << "error loading file!" << std::endl;
+    }
+    wizard_sprite.setTexture(wizard_texture);
+    wizard_sprite.setScale(2.f, 2.f);
+
+    window.create(sf::VideoMode(800 * 2, 600 * 2), "Green Dragon");
 
     while (window.isOpen())
     {
@@ -13,6 +23,14 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here
+        // window.draw()
+        window.draw(wizard_sprite);
+        // end the current frame
+        window.display();
     }
     return 0;
 }
